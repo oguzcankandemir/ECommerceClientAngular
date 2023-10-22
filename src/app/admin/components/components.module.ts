8908db9976ab094166ed1f5685c29eafb3c0223c
oğuzcan kandemir <oguzcankandemir@hotmail.com>
2023-10-23 02:51:35 +0300

    [feature] with Angular page operations done.

---
+++ src/app/admin/components/components.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductsComponent } from './products/products.component';
+import { ProductsModule } from './products/products.module';
+import { OrderModule } from './order/order.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { CustomerModule } from './customer/customer.module';
+
+
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule,
+    ProductsModule,
+    OrderModule,
+    DashboardModule,
+    CustomerModule
+  ]
+})
+export class ComponentsModule { }
