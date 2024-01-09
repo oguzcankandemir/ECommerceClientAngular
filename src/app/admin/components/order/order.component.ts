849b78d80d84ab72f6c1ae5ed3db6fc755e463ce
oguzcankandemir <oguzcankandemir@hotmail.com>
2024-01-09 22:10:11 +0300

    [feature] Animation Waiting Infrastructure with Spinner done.

--- src/app/admin/components/order/order.component.ts
+++ src/app/admin/components/order/order.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';  
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
   styleUrls: ['./order.component.scss']
 })
-export class OrderComponent {
-
+export class OrderComponent implements OnInit {
+constructor(private spinner: NgxSpinnerService){}
+ngOnInit(): void{
+  this.spinner.show();
+}
 }
